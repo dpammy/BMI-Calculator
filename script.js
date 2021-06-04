7b6c3f1bfd49346c7d9fc583462c9675121132b2
dpammy <pamela.dianon@amalitech.org>
2021-06-04 13:09:38 +0100

    modified logic in script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function BMI() {
   } else if ((bmi >= 18.5) && (bmi <= 24.9)) {
     document.getElementById("result").innerHTML =
       "Your BMI is " + bmi + ", so you are normal.";
-  } else if ((bmi > 24.9) && (bmi >= 29.9)) {
+  } else if ((bmi > 24.9) && (bmi <= 29.9)) {
     document.getElementById("result").innerHTML =
       "Your BMI is " + bmi + ", so you are overweight.";
   } else if ((bmi > 29.9) && (bmi <= 34.9)) {
